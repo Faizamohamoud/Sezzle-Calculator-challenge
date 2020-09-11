d57d1e16f11f27a25ad13e6a6eea2d7ad674978a
faiza <faizamohamoud@gmail.com>
2020-09-11 01:23:34 -0500

    changes to package.json, adding npm start

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ router.get('/refresh', function (req, res) {
 
 app.use('/', router);
 app.use('/', express.static(__dirname + '/src/static'));
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
